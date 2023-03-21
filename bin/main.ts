9c6e7eb5218a4fc3e95ec9a60f5ff0861e3e2905
mototsuka <originbenntou8973@gmail.com>
2023-03-21 21:39:43 +0900

    commit

--- bin/main.ts
+++ bin/main.ts
@@ -1,12 +1,4 @@
 #!/usr/bin/env node
-import 'source-map-support/register'
-import * as cdk from 'aws-cdk-lib'
 import { YourApp } from '../lib/your-app'
 
-const app = new cdk.App()
-new YourApp(app, 'YourApp', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-})
+new YourApp().synth()
--- lib/your-app.ts
+++ lib/your-app.ts
@@ -1,17 +1,16 @@
 import * as cdk from 'aws-cdk-lib'
-import { Construct } from 'constructs'
 import { ApiGatewayStack } from './api-gateway-stack'
 import { LambdaStack } from './lambda-stack'
 
-export class YourApp extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props)
+export class YourApp extends cdk.App {
+  constructor() {
+    super()
 
     const apiGatewayStack = new ApiGatewayStack(this, 'ApiGatewayStack')
     const lambdaStack = new LambdaStack(this, 'LambdaStack', {
       apiGw: apiGatewayStack.apiGw,
     })
 
-    // lambdaStack.addDependency(apiGatewayStack)
+    apiGatewayStack.addDependency(lambdaStack)
   }
 }

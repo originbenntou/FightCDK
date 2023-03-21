17b31c3a4d800a3328928807f4820adad75605c3
mototsuka <originbenntou8973@gmail.com>
2023-03-21 22:24:16 +0900

    commit

--- bin/main.ts
+++ bin/main.ts
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
-import { YourApp } from '../lib/your-app'
+import * as cdk from 'aws-cdk-lib'
+import { ApiGatewayStack } from '../lib/api-gateway-stack'
+import { LambdaStack } from '../lib/lambda-stack'
 
-new YourApp().synth()
+const app = new cdk.App()
+
+const apiGatewayStack = new ApiGatewayStack(app, 'ApiGatewayStack')
+const lambdaStack = new LambdaStack(app, 'LambdaStack', {
+  apiGw: apiGatewayStack.apiGw,
+})
+apiGatewayStack.addDependency(lambdaStack)
+
+app.synth()
